922cb089dae1e4e1733d981e73b8c95ff34e544f
Siarhei Rachytski <siarhei.rachytski@gmail.com>
2020-05-16 18:11:00 +0100

    added pinout.h as a dispatching header for pin mapping and pinout-arduino-nano.h as a first supported platform

---
+++ include/pinout.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#define ARDUINO_NANO
+
+// Physical pinout of the soldering station.
+// Specific layout is selected here based on the platform define 
+
+#ifdef ARDUINO_NANO
+
+#include "pinout_arduino_nano.hpp"
+
+#endif
+
+// add more platforms here if necessary
---
+++ include/pinout_arduino_nano.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+// physical pinout for the Arduino Nano
+
+// control pins
+#define HEATING_GUN_COOLING_FAN 2
+#define SOLDERING_IRON_HEATING_ELEMENT 3
+#define HEATING_GUN_HEATING_ELEMENT 4
+
+// rotary encoder pins
+#define SW_PIN 6
+#define DT_PIN 5
+#define CLK_PIN 13
+
+// analogue pins for thermal sensors
+#define HEATING_GUN_TEMP_SENSOR0 3
+#define SOLDERING_IRON_TEMP_SENSOR0 4
